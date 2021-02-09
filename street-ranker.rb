fb912c8a9adeb0bedac320171adc3272167579a7
Bilal Khalid <bkbilalarain@gmail.com>
2021-02-09 14:24:49 -0500

    Improve the web scraper
    Collects and prints fighter play rate data for ALL published dates

--- street-ranker.rb
+++ street-ranker.rb
@@ -5,6 +5,16 @@ language="en"
 url = "https://game.capcom.com/cfn/sfv/stats/usagerate?lang=#{language}"
 html = URI.open(url)
 response = Nokogiri::HTML(html)
-names = response.css("p").select{|name| name["class"] == "name"}
-playRate = response.css("p").select{|name| name["class"] == "percent"}
-(0..names.size-1).each{|i| puts "#{names[i].text}: #{playRate[i].text}"}
+dateList = response.css("#selectColumn option")
+dateList.each{
+    |date|
+    textDate = date.text
+    urlDate = date.attribute("value").value
+    url = "https://game.capcom.com/cfn/sfv/stats/usagerate/#{urlDate}?lang=#{language}"
+    html = URI.open(url)
+    response = Nokogiri::HTML(html)
+    names = response.css(".name")
+    playRate = response.css(".percent")
+    puts textDate+"\n"
+    (0..names.size-1).each{|i| puts "#{names[i].text}: #{playRate[i].text.to_f}"}
+}
