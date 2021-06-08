450b4bd94eb7369108cc7d14028ec94b4e53f760
bkhal <bkbilalarain@gmail.com>
2021-06-08 18:26:27 -0400

    created JSON object

--- street_ranker.rb
+++ street_ranker.rb
@@ -2,12 +2,14 @@
 
 require 'open-uri'
 require 'nokogiri'
+require 'json'
 
 language = 'en'
 url = "https://game.capcom.com/cfn/sfv/stats/usagerate?lang=#{language}"
 html = URI.parse(url).open
 response = Nokogiri::HTML(html)
 date_list = response.css('#selectColumn option')
+json_builder = String.new('{')
 date_list.each do |date|
   text_date = date.text
   url_date = date.attribute('value').value
@@ -16,6 +18,10 @@ date_list.each do |date|
   response = Nokogiri::HTML(html)
   names = response.css('.name')
   play_rate = response.css('.percent')
-  puts text_date
-  (0..names.size - 1).each { |i| puts "#{names[i].text}: #{play_rate[i].text.to_f}" }
+  json_builder.concat("\"#{text_date}\": {")
+  (0..names.size - 1).each { |i| json_builder += "\"#{names[i].text}\": #{play_rate[i].text.to_f}," }
+  json_builder.delete_suffix!(',').concat('},')
 end
+json_builder.delete_suffix!(',').concat('}')
+data = JSON.parse(json_builder)
+puts data
