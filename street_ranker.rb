f7a8b131694732145dcaf4a0333ecd907fdc2903
bkhal <bkbilalarain@gmail.com>
2021-06-14 19:10:50 -0400

    exports data to local Elasticsearch instance

--- street_ranker.rb
+++ street_ranker.rb
@@ -3,14 +3,18 @@
 require 'open-uri'
 require 'nokogiri'
 require 'json'
+require 'elasticsearch'
+
+client = Elasticsearch::Client.new log: true
+client.cluster.health
 
 language = 'en'
 url = "https://game.capcom.com/cfn/sfv/stats/usagerate?lang=#{language}"
 html = URI.parse(url).open
 response = Nokogiri::HTML(html)
 date_list = response.css('#selectColumn option')
-json_builder = String.new('{')
-date_list.each do |date|
+q = 1
+date_list.reverse.each do |date|
   text_date = date.text
   url_date = date.attribute('value').value
   url = "https://game.capcom.com/cfn/sfv/stats/usagerate/#{url_date}?lang=#{language}"
@@ -18,10 +22,11 @@ date_list.each do |date|
   response = Nokogiri::HTML(html)
   names = response.css('.name')
   play_rate = response.css('.percent')
-  json_builder.concat("\"#{text_date}\": {")
+  json_builder = String.new("{\"#{text_date}\": {")
+  puts json_builder
   (0..names.size - 1).each { |i| json_builder += "\"#{names[i].text}\": #{play_rate[i].text.to_f}," }
-  json_builder.delete_suffix!(',').concat('},')
+  json_builder.delete_suffix!(',').concat('}}')
+  data = JSON.parse(json_builder)
+  client.index(index: "streetranker_#{text_date.delete('/').downcase}", id: q, body: data)
+  q += 1
 end
-json_builder.delete_suffix!(',').concat('}')
-data = JSON.parse(json_builder)
-puts data
