bc92bdab433b0f7fff23f7c8431f966b088f4f0c
bkhal <bkbilalarain@gmail.com>
2021-06-15 13:21:14 -0400

    skips data export if index already exists

--- street_ranker.rb
+++ street_ranker.rb
@@ -9,17 +9,24 @@ client = Elasticsearch::Client.new log: true
 client.cluster.health
 
 language = 'en'
-url = "https://game.capcom.com/cfn/sfv/stats/usagerate?lang=#{language}"
+url = "https://game.capcom.com/cfn/sfv/stats/usagerate?lang=#{language}" # URL to pull Street Fighter V stats from
 html = URI.parse(url).open
 response = Nokogiri::HTML(html)
 date_list = response.css('#selectColumn option')
-q = 1
+q = 0
+
+# iterates through every month in dropdown menu
 date_list.reverse.each do |date|
+  q += 1
   text_date = date.text
+  next if client.exists(index: "streetranker_#{text_date.delete('/').downcase}", id: q) # next loop if index exists
+
   url_date = date.attribute('value').value
   url = "https://game.capcom.com/cfn/sfv/stats/usagerate/#{url_date}?lang=#{language}"
   html = URI.parse(url).open
   response = Nokogiri::HTML(html)
+
+  # sends every 'Character: Play rate' pair from selected month to a new Elasticsearch index
   names = response.css('.name')
   play_rate = response.css('.percent')
   json_builder = String.new("{\"#{text_date}\": {")
@@ -28,5 +35,4 @@ date_list.reverse.each do |date|
   json_builder.delete_suffix!(',').concat('}}')
   data = JSON.parse(json_builder)
   client.index(index: "streetranker_#{text_date.delete('/').downcase}", id: q, body: data)
-  q += 1
 end
