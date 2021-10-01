57f8cfad9f6b057818e509eeeb437536426dbb99
bkhal <bkbilalarain@gmail.com>
2021-10-01 14:35:07 -0400

    changes document id for more intuitive querying

--- street_ranker.rb
+++ street_ranker.rb
@@ -12,13 +12,12 @@ url = "https://game.capcom.com/cfn/sfv/stats/usagerate?lang=#{language}" # URL t
 html = URI.parse(url).open
 response = Nokogiri::HTML(html)
 date_list = response.css('#selectColumn option')
-id = 0
 
 # iterates through every month in dropdown menu
 date_list.reverse.each do |dates|
   url_date = dates.attribute('value').value
   url = "https://game.capcom.com/cfn/sfv/stats/usagerate/#{url_date}?lang=#{language}"
-  formatted_date = url_date.to_s.insert(4, '-')
+  formatted_date = String.new(url_date.to_s).insert(4, '-')
   html = URI.parse(url).open
   response = Nokogiri::HTML(html)
   names = response.css('.name')
@@ -28,7 +27,7 @@ date_list.reverse.each do |dates|
     json_builder = "{\"Character\": \"#{names[i].text}\",\"Playrate\": #{play_rate[i].text.to_f},\
     \"Month\": \"#{formatted_date}\"}"
     data = JSON.parse(json_builder)
+    id = names[i].text + url_date.to_s
     client.index(index: 'street_ranker', id: id, body: data)
-    id += 1
   end
 end
